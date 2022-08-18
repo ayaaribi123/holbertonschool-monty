9e923978aaf685aa1f2351495027e7d0ba515b89
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2022-08-18 12:32:03 +0100

    swap

---
+++ swap.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+*swap - main elements
+*@stack: pointer
+*@line_number: line number
+*Return: exit SUCCESS on success or FAILURE on fail
+*/
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *t;
+	if ((*stack) != NULL && (*stack)->next == NULL)
+	{
+		printf(stderr,"L%d: can't swap, stack too short", line_number);
+	}
+	t = (*stack)->next;
+	(*stack)->prev = (*stack)->next;
+	(*stack)->next = t;
+	if (t->next)
+	{
+		t->next->prev = (*stack)->next;
+		t->next = (*stack)->next;
+		(*stack) = (*stack)->prev;
+	}
+}
