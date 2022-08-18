6151d361590cca2a58fad22b265fbd42bafd80ec
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2022-08-18 12:07:29 +0100

    monty.h

--- monty.h
+++ monty.h
@@ -58,6 +58,10 @@ int main(int argc, char **argv);
 void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nope(stack_t **stack, unsigned int line_number);
 
 #endif
