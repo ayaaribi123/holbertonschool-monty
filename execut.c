7bea2adf65e6237f9aaad144e121e487819bf4d2
dhaferHS <dhafer.darkprod@gmail.com>
2022-08-18 03:12:41 +0100

    execut.c

---
+++ execut.c
@@ -0,0 +1,24 @@
+#include"monty.h"
+/**
+ * execute_opcode - call opcode of every function
+ * @opcode: opcode that will be passed to function
+ * @stack: pointer to the stack
+ * @line_number: line numbers
+ * 
+ */
+void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number)
+{
+int i;
+instruction_t opcodes[] = OPCODES;
+while (opcodes[i].opcode)
+{
+if (strcmp(opcodes[i].opcode, opcode) == 0)
+{
+opcodes[i].f(stack, line_number);
+return;
+}
+i++;
+}
+fprintf(stderr, "L%d: inknown instruction %s\n", line_number, opcode);
+exit(EXIT_FAILURE);
+}
