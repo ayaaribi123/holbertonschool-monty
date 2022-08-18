bcddbab5326bc80aa6e9136d0d8002c054ca7736
dhaferHS <dhafer.darkprod@gmail.com>
2022-08-18 04:04:34 +0100

    pall.c

--- pall.c
+++ pall.c
@@ -6,13 +6,13 @@
  * @line_number:line number
  * 
  */
-void pall(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number)
 {
 int lengeth = 0;
 (void) line_number;
 while (*stack != NULL)
 {
-printf("%d\n", (*stack)->n)
+printf("%d\n", (*stack)->n);
 ++lengeth;
 *stack = (*stack)->next;
 }
