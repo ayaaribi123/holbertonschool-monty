892a4d085901cd362eafc6370df2a8e5d5d0c6c5
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2022-08-18 12:40:09 +0100

    add

---
+++ add.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+*add - main elements
+*@stack: pointer
+*@line_number: line number
+*Return: exit SUCCESS on success or FAILURE on fail
+*/
+void add(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next = NULL || (*stack)->next = NULL)
+	{
+			fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+			exit(EXIT_FAILURE);
+	}
+	pop(stack, line_number)
+	(*stack)->n += (*stack)->n;
+}
