9541d43cc4e9bcfa368f32cb9a4ceb531c87239e
dhaferHS <dhafer.darkprod@gmail.com>
2022-08-18 03:03:40 +0100

    pall.c

---
+++ pall.c
@@ -0,0 +1,19 @@
+#include"monty.h"
+
+/**
+ * pall - prints all the values on the stack, starting from the top of the stack
+ * @stack: pointer to the stack
+ * @line_number:line number
+ * 
+ */
+void pall(stack_t **stack, unsigned int line_number);
+{
+int lengeth = 0;
+(void) line_number;
+while (*stack != NULL)
+{
+printf("%d\n", (*stack)->n)
+++lengeth;
+*stack = (*stack)->next;
+}
+}
