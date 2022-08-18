44479e38318e0ed21328014f62562abcf5c7fb27
dhaferHS <dhafer.darkprod@gmail.com>
2022-08-18 03:03:56 +0100

    monty.h

--- monty.h
+++ monty.h
@@ -50,6 +50,9 @@ typedef struct instruction_s
 		{NULL, NULL}\
 	}
 int main(int argc, char **argv);
+void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
 
 #endif
