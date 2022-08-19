a3bbe4fc6145aff1ae79bee99701040dcf5fe1f6
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2022-08-19 21:15:40 +0100

    main.c

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include "monty.h"
 /**
- *main - main elements
- *
- *@argc: argument counting
- *@argv: argument value
- *return: exit SUCCESS on success or FAILURE on fail
- */
-
+*main - main elements
+*@argc: argument counting
+*@argv: argument value
+*return: exit SUCCESS on success or FAILURE on fail
+**/
 int main(int argc, char **argv)
 {
 FILE *fd;
@@ -18,14 +16,12 @@ stack_t *STACK, *temp;
 (void) argc;
 
 STACK = NULL;
-
 if (argc != 1)
 {
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
 fd = fopen(argv[1], "r");
-
 if (!fd)
 {
 fprintf(stderr, "Error: Can't open file %s \n", argv[1]);
@@ -33,13 +29,11 @@ exit(EXIT_FAILURE);
 }
 while ((getline(&line, &size, fd)) != (-1))
 {
-
 if (*line == '\n')
 {
 line_number++;
 continue;
 }
-
 opcode = strtok(NULL, "\n");
 if (!opcode)
 {
@@ -49,20 +43,18 @@ continue;
 Arg.argument = strtok(NULL, "\n");
 execute_opcode(opcode, &STACK, line_number);
 line_number++;
-
 free(line);
 if (STACK != NULL)
 {
 	while (STACK != NULL)
 {
-temp = STACK;
-STACK = temp->next;
-free(temp);
+		temp = STACK;
+		STACK = temp->next;
+		free(temp);
 }
 }
 fclose(fd);
 exit(EXIT_SUCCESS);
-
 }
-return EXIT_SUCCESS;
+return (EXIT_SUCCESS);
 }
