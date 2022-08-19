2bfa4f129e13409350bbd45f60c8eea9b3428990
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2022-08-19 21:38:52 +0100

    nope

--- nope.c
+++ nope.c
@@ -3,7 +3,7 @@
  *nope - main elements
  *@stack: pointer
  *@line_number: line number
- *Return: exit SUCCESS on success or FAILURE on fail
+ *Return: nothing
  */
 void nope(stack_t **stack, unsigned int line_number)
 {
