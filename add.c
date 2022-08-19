046b3ce7e74a5926bd1286a3c2a025f755172eec
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2022-08-19 20:39:31 +0100

    add

--- add.c
+++ add.c
@@ -9,9 +9,10 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-			fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-			exit(EXIT_FAILURE);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
 	}
+
 	pop(stack, line_number);
 	(*stack)->n += (*stack)->n;
 }
