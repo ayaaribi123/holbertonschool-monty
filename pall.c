129e4f633f74652e81fb7ad9c5f7422aa952f5b2
ayaaribi <93769004+ayaaribi123@users.noreply.github.com>
2022-08-19 21:24:07 +0100

    pall

--- pall.c
+++ pall.c
@@ -1,11 +1,11 @@
 #include"monty.h"
 
 /**
- * pall - prints all the values on the stack, starting from the top of the stack
- * @stack: pointer to the stack
- * @line_number:line number
- * 
- */
+*pall - print all the values on the stack, start from the top of the stack
+*@stack: pointer to the stack
+*@line_number:line number
+*
+*/
 void pall(stack_t **stack, unsigned int line_number)
 {
 int lengeth = 0;
