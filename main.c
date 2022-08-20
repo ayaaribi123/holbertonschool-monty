ee8a4f0dcefc30341bc7a96a81b44abed6826f91
dhaferHS <dhafer.darkprod@gmail.com>
2022-08-20 01:21:11 +0100

    main.c

--- main.c
+++ main.c
@@ -1,62 +1,72 @@
 #include "monty.h"
+
 /**
-*main - main elements
-*@argv: argument counting
-*@argc: argument value
-*Return: exit SUCCESS on success or FAILURE on fail
-**/
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: exit SUCCESS on success, FAILURE on fail
+ */
+
 int main(int argc, char **argv)
 {
-FILE *fd;
-char *line = NULL;
-size_t size;
-char *opcode = NULL;
-unsigned int line_number;
-stack_t *STACK, *temp;
-(void) argc;
+	FILE *fd;
+	char *line = NULL;
+	size_t size = 0;
+	char *opcode = NULL;
+	unsigned int line_number = 1; /* Monty starts counting lines at 1 */
+	stack_t *STACK, *temp;
+	(void) argc; /* temporary */
 
-STACK = NULL;
-if (argc != 1)
-{
-fprintf(stderr, "USAGE: monty file\n");
-exit(EXIT_FAILURE);
-}
-fd = fopen(argv[1], "r");
+	STACK = NULL;
 
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-if (fd == NULL)
-{
-fprintf(stderr, "Error: Can't open file %s \n", argv[1]);
-exit(EXIT_FAILURE);
-}
-while ((getline(&line, &size, fd)) != (-1))
-{
-if (*line == '\n')
-{
-line_number++;
-continue;
-}
-opcode = strtok(NULL, "\n");
-if (!opcode)
-{
-line_number++;
-continue;
-}
-Arg.argument = strtok(NULL, "\n");
-execute_opcode(opcode, &STACK, line_number);
-line_number++;
-free(line);
-if (STACK != NULL)
-{
-	while (STACK != NULL)
-{
-		temp = STACK;
-		STACK = temp->next;
-		free(temp);
-}
-}
-fclose(fd);
-exit(EXIT_SUCCESS);
-}
-return (EXIT_SUCCESS);
+	fd = fopen(argv[1], "r"); /* open file */
+
+	if (!fd) /* error if cannot open file - maybe using fprintf */
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((getline(&line, &size, fd)) != (-1)) /* read each line */
+	{
+		if (*line == '\n') /* check for blank line */
+		{
+			line_number++;
+			continue;
+		}
+		opcode = strtok(line, " \t\n"); /* set opcode to first token of strtok */
+
+		if (!opcode) /* check for strtok fail */
+		{
+			line_number++;
+			continue;
+		}
+		Arg.argument = strtok(NULL, " \t\n");
+
+		execute_opcode(opcode, &STACK, line_number);
+		line_number++;
+	}
+
+	free(line); /* free line */
+
+	if (STACK != NULL) /* free stack */
+	{
+		while (STACK != NULL)
+		{
+			temp = STACK;
+			STACK = temp->next;
+			free(temp);
+		}
+	}
+
+	fclose(fd); /* close file */
+
+	exit(EXIT_SUCCESS); /* exit */
 }
